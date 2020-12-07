28e178b1cc7e6992f5fead90a3162b66314d0d3b
Marco Spitzbarth <bulla@ibulla.com>
2020-12-07 15:37:33 +0100

    Update slozkimachine.sol

--- slozkimachine.sol
+++ slozkimachine.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.7.0 <0.8.0;
+pragma solidity ^0.5.0;
 
 contract SzaboMachine {
     
     uint256 unlockBlock;
     
-    constructor () payable {
-    uint256 currBlock = block.number;
-    uint256 freeBlocks = msg.value / 1000000000000000;
-      if(unlockBlock > currBlock){
+    function () external payable {
+        uint256 currBlock = block.number;
+        uint256 freeBlocks = msg.value / 1000000000000000;
+        if(unlockBlock > currBlock){
             unlockBlock = unlockBlock + freeBlocks;
         }else{
             unlockBlock = currBlock + freeBlocks;
